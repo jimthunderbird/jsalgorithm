a1b6567ebe9e8c825a446f9996aa5f1f1f2cc099
jim zike huang <jim.javathunderbird@gmail.com>
2017-09-08 08:58:53 -0700

    update comments

--- trap_rain_water.js
+++ trap_rain_water.js
@@ -12,17 +12,7 @@
  *
  * Time complexity: O(N)
  * Space Complexity: O(1)
- *
- * initial: maxH = 0, delta = maxH = 3;
- * i = 0, h[i] = 0, maxH = max(h[i],maxH) = 0, delta = maxH - curMaxHeight = 3 - 0 = 3, store: maxH - (maxH - curMaxHeight) - h[i] = 3 - 3 - 0 = 0
- * i = 1, h[i] = 1,maxH = max(h[i],maxH) = 1, delta = delta - maxH = 3 - 1 = 2,store: maxH - 2 - 1 = 0
- * i = 2, h[i] = 0, maxH = max(h[i],maxH) = 1, delta = delta - maxH = 3 - 1 = 2,store: maxH - delta - h[i] = 3 - 2 - 0 = 1
- * i = 3, h[i] = 2, maxH = max(h[i],maxH) = max(2,1) = 2, delta = delta - maxH = 3 - 2 = 1,store: maxH - 1 - 2 = 0
- * i = 4, h[i] = 1, maxH = max(h[i],maxH) = max(1,2) = 2, delta = delta - maxH = 3 - 2 = 1,store: maxH - delta - h[i] = 3 - 1 - 1 = 1
- * i = 5, h[i] = 0, maxH = max(h[i],maxH) = max(0,2) = 2, delta = delta - maxH = 3 - 2 = 1,store: maxH - delta - h[i] = 3 - 1 - 0 = 2
- * i = 6, h[i] = 1, maxH = max(h[i],maxH) = max(1,2) = 2, delta = delta - maxH = 3 - 2 = 1,store: maxH - delta - h[i] = 3 - 1 - 1 = 1
- * ----------------------------------------------------------------------------------------------------------------
-*/
+ */
 
 function trap(height) {
   //find the max height and its index
@@ -61,4 +51,3 @@ function trap(height) {
 const height = [0,1,0,2,1,0,1,3,2,1,2,1];
 
 console.log(trap(height));
-
